228bd57b2155e83c509136d37760af1a7854cdb4
Jacques De SAGAN <jacquesde@sagan.me>
2016-04-16 14:01:16 +0800

    add support for link in AREA element

--- src/content.js
+++ src/content.js
@@ -1,10 +1,10 @@
 
 window.addEventListener('click', (e) => {
     let i = 0, el = e.target;
-    while( el && el.tagName != 'A' && i++ < 3 ) {
+    while( el && el.tagName != 'A' && el.tagName != 'AREA' && i++ < 3 ) {
         el = el.parentNode;
     }
-    if( el && el.tagName == 'A' && el.target == "_blank" ) {
+    if( el && el.target == "_blank" ) {
         el.target = "_self";
     }
 }, true);
