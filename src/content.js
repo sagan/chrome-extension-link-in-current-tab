c7924c9c2fa574403ebd110f377dcc4b986ac1b9
Jacques De SAGAN <jacquesde@sagan.me>
2016-05-01 15:36:14 +0800

    fix problem in some websites

--- src/content.js
+++ src/content.js
@@ -1,5 +1,8 @@
 
-window.addEventListener('click', (e) => {
+window.addEventListener('click', handle);
+window.addEventListener('click', handle, true);
+
+function handle(e) {
     let i = 0, el = e.target;
     while( el && el.tagName != 'A' && el.tagName != 'AREA' && i++ < 3 ) {
         el = el.parentNode;
@@ -7,4 +10,4 @@ window.addEventListener('click', (e) => {
     if( el && el.target == "_blank" ) {
         el.target = "_self";
     }
-});
+}
