58bdbc237f198bdf86cff7a4d3cdf0dbae3ffd8e
Jacques De SAGAN <jacquesde@sagan.me>
2016-05-11 14:50:03 +0800

    add support for form submit target

--- src/content.js
+++ src/content.js
@@ -1,10 +1,12 @@
 
 window.addEventListener('click', handle);
 window.addEventListener('click', handle, true);
+window.addEventListener('submit', handle);
+window.addEventListener('submit', handle, true);
 
 function handle(e) {
     let i = 0, el = e.target;
-    while( el && el.tagName != 'A' && el.tagName != 'AREA' && i++ < 3 ) {
+    while( el && !el.target && i++ < 3 ) {
         el = el.parentNode;
     }
     if( el && el.target == "_blank" ) {
