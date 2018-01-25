53cc87881656b0696b716f8616af438b9ebf7bc1
Jacques De SAGAN <jacquesde@sagan.me>
2018-01-25 11:56:56 +0800

    update to block frame name target also

--- src/content.js
+++ src/content.js
@@ -5,11 +5,24 @@ window.addEventListener('submit', handle);
 window.addEventListener('submit', handle, true);
 
 function handle(e) {
-    let i = 0, el = e.target;
-    while( el && !el.target && i++ < 3 ) {
-        el = el.parentNode;
+  let i = 0, el = e.target;
+  let frames = Array.from(document.querySelectorAll('iframe,frame')).map(f => f.name).filter(f => f);
+  try {
+    if( window.parent != window ) {
+      frames.push(...Array.from(window.parent.document.querySelectorAll('iframe,frame')).map(f => f.name).filter(f => f));
     }
-    if( el && el.target == "_blank" ) {
+  } catch(e) {}
+
+  while( el && !el.target && i++ < 5 ) {
+    el = el.parentNode;
+  }
+  if( el && el.target ) {
+    if( el.target.startsWith('_') ) {
+      if( el.target == "_blank" ) {
         el.target = "_top";
+      }
+    } else if( !frames.includes(el.target) ) {
+      el.target = "_top";
     }
+  }
 }
